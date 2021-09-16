46ba178190e2504cbc541202413f07c3c700feca
mbmukesh <mbjino@gmail.com>
2021-09-16 17:16:17 +0530

    added http code

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const { names, ultimate } = require("./module1");
 const animes = require("./module2");
-const os = require("os");
-const fs = require("fs");
-
+const os    = require("os");
+const fs    = require("fs");
+const http  = require("http");
 /*
 console.log(names);
 console.log(animes);
@@ -17,8 +17,24 @@ fs.writeFile("./assets/text.txt", "im so fast master", () => {
     console.log("done sir")
 })
 
-*/
+Aynschornous 
 
-console.log(fs.readFileSync("./assets/text.txt").toString()); // I'm soooo fast
+console.log(fs.readFileSync("./assets/text.txt").toString());
 
 fs.writeFileSync("./assets/text.txt", "I'm actually faster");
+*/
+
+const HOSTNAME = "localhost";
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log(req);
+    console.log(res);
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Hello from the Server!");
+    res.end();
+});
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`SERVER STATER ON http://${HOSTNAME}:${PORT}`);
+})
